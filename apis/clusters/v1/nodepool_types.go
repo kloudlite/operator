d82245c48a23c7fe9612c17c6b8e52deaef4852f
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-06-22 15:17:49 +0530

    :label: Fixed some types to make more clear
    updated some types inside nodepool of operator clusters for the awsnodeconfig.

--- apis/clusters/v1/nodepool_types.go
+++ apis/clusters/v1/nodepool_types.go
@@ -7,14 +7,53 @@ import (
 	rApi "github.com/kloudlite/operator/pkg/operator"
 )
 
+type ProvisionMode string
+
+const (
+	ProvisionModeOnDemand ProvisionMode = "on-demand"
+	ProvisionModeSpot     ProvisionMode = "spot"
+	ProvisionModeReserved ProvisionMode = "reserved"
+)
+
+type SpotSpecs struct {
+	CpuMin int `json:"cpuMin"`
+	CpuMax int `json:"cpuMax"`
+	MemMin int `json:"memMin"`
+	MemMax int `json:"memMax"`
+}
+
+type OnDemandSpecs struct {
+	InstanceType string `json:"instanceType"`
+}
+
+type AWSNodeConfig struct {
+	NadeName      *string        `json:"nodeName"`
+	OnDemandSpecs *OnDemandSpecs `json:"onDemandSpecs"`
+	SpotSpecs     *SpotSpecs     `json:"spotSpecs"`
+	VPC           *string        `json:"vpc"`
+	Region        *string        `json:"region"`
+	ImageId       *string        `json:"imageId"`
+	IsGpu         *bool          `json:"isGpu"`
+	ProvisionMode ProvisionMode  `json:"provisionMode" enum:"on-demand;spot;reserved;"`
+}
+
+type CloudProvider string
+
+const (
+	CloudProviderAWS CloudProvider = "aws"
+	CloudProviderGCP CloudProvider = "gcp"
+)
+
 // NodePoolSpec defines the desired state of NodePool
 type NodePoolSpec struct {
 	MaxCount    int `json:"maxCount"`
 	MinCount    int `json:"minCount"`
 	TargetCount int `json:"targetCount"`
 
+	AWSNodeConfig *AWSNodeConfig `jons:"awsNodeConfig"`
+
 	// aws -> CloudProvider
-	NodeConfig string `json:"nodeConfig"`
+	// NodeConfig string `json:"nodeConfig"`
 
 	// IsStateful bool `json:"isStateful,omitempty"`
 
