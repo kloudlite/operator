379c4e7ab58574d3f8b8485d1710958d0bdca93f
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-16 13:01:23 +0530

    feat(platform-operator): includes remaining operators

--- cmd/platform-operator/main.go
+++ cmd/platform-operator/main.go
@@ -2,30 +2,35 @@ package main
 
 import (
 	"github.com/kloudlite/operator/operator"
-	// app "github.com/kloudlite/operator/operators/app-n-lambda/controller"
+	app "github.com/kloudlite/operator/operators/app-n-lambda/controller"
+	clusters "github.com/kloudlite/operator/operators/clusters/controller"
+	helmCharts "github.com/kloudlite/operator/operators/helm-charts/controller"
+	msvcMongo "github.com/kloudlite/operator/operators/msvc-mongo/controller"
+	msvcAndMres "github.com/kloudlite/operator/operators/msvc-n-mres/controller"
+	nodepool "github.com/kloudlite/operator/operators/nodepool/controller"
+	project "github.com/kloudlite/operator/operators/project/controller"
 	resourceWatcher "github.com/kloudlite/operator/operators/resource-watcher/controller"
+	// routers "github.com/kloudlite/operator/operators/routers/controller"
 )
 
 func main() {
 	mgr := operator.New("platform-operator")
 
 	// kloudlite resources
-	// app.RegisterInto(mgr)
-	// project.RegisterInto(mgr)
-	// helmCharts.RegisterInto(mgr)
+	app.RegisterInto(mgr)
+	project.RegisterInto(mgr)
+	helmCharts.RegisterInto(mgr)
 	// routers.RegisterInto(mgr)
-	//
-	// // kloudlite managed services
-	// msvcAndMres.RegisterInto(mgr)
-	// msvcMongo.RegisterInto(mgr)
-	// msvcRedis.RegisterInto(mgr)
-	// msvcRedpanda.RegisterInto(mgr)
-	//
-	// // kloudlite cluster management
-	// clusters.RegisterInto(mgr)
-	// nodepool.RegisterInto(mgr)
-	//
-	// // kloudlite resource status updates
+
+	// kloudlite managed services
+	msvcAndMres.RegisterInto(mgr)
+	msvcMongo.RegisterInto(mgr)
+
+	// kloudlite cluster management
+	clusters.RegisterInto(mgr)
+	nodepool.RegisterInto(mgr)
+
+	// kloudlite resource status updates
 	resourceWatcher.RegisterInto(mgr, false)
 
 	mgr.Start()
